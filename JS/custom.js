cb7560f5ea02a64aea3fc0c9317fe7fc8cff06de
Yash Nag <hsayganalpha@gmail.com>
2017-06-07 08:59:49 +0530

    Update custom.js

--- JS/custom.js
+++ JS/custom.js
@@ -1,5 +1,30 @@
+/*//Moonwalk Animation
+$("#moonwalk").animate({"margin-right": "+=2000px"}, 10000);
+do
+{
+    var lm = parseInt($('#moonwalk').css('margin-left'), 10);
+    lm = lm + 10;
+    $("#moonwalk").css("margin-left",lm+"px");
+} while (lm < 100);*/
+
+
 //Smooth Scolling
 $('a[href*=\\#]').on('click', function(event) {
     event.preventDefault();
     $('html,body').animate({scrollTop:$(this.hash).offset().top}, 1000);
 });
+
+//Slide Animation
+$(document).ready(function(){
+
+  $(window).scroll(function() {
+    $(".slideanim").each(function(){
+      var pos = $(this).offset().top;
+
+      var winTop = $(window).scrollTop();
+        if (pos < winTop + 600) {
+          $(this).addClass("slide");
+        }
+    });
+  });
+})
